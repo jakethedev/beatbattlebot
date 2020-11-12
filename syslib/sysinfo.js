c5ba5f8244c6a4670c7e85314df2df5fa8318bef
jakethedev <16889000+jakethedev@users.noreply.github.com>
2020-11-12 15:02:51 -0700

    Cleanup config defaults to be better, fix sysinfo rand usage

--- syslib/sysinfo.js
+++ syslib/sysinfo.js
@@ -1,6 +1,6 @@
 const os = require('os')
 const MBinB = 1048576
-require('../randomUtil')
+const rand = require('../randomUtil')
 
 // Simple version, bug link, and release notes output
 exports.version = function() {
@@ -15,14 +15,14 @@ exports.feedback = function() {
 }
 
 exports.serverstats = function() {
-  if (randIntMinOne(50) == 50) {
-    return choice([`I have no memory of this place`, `get me out of here!`, `life's good, the kids are well. How are you?`, `there has been an anomaly`])
+  if (rand.randIntMinOne(50) == 50) {
+    return rand.choice([`I have no memory of this place`, `get me out of here!`, `life's good, the kids are well. How are you?`, `there has been an anomaly`])
   } else {
     let load = os.loadavg().map((val) => val.toPrecision(2))
     let free = Math.round(os.freemem() / MBinB)
     let max = Math.round(os.totalmem() / MBinB)
     let uptime = Math.round(os.uptime() / 3600.0)
-    let desire = choice(['raise', 'smoothie', 'piece of cake', 'new synth plugin', 'massage', 'day off', 'new manager', 'new xlr cable', 'hammer and some balloons'])
+    let desire = rand.choice(['raise', 'smoothie', 'piece of cake', 'new synth plugin', 'massage', 'day off', 'new manager', 'new xlr cable', 'hammer and some balloons'])
     return `I've been awake for ${uptime} hours, my workload looks like ${load}, I've got ${free} MB free of ${max}, and I really want a ${desire} - thanks for asking.`
   }
 }
