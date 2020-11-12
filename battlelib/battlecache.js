3bd544981edd7b1896a61816c52aa3505e64a09c
jakethedev <16889000+jakethedev@users.noreply.github.com>
2020-11-12 15:45:01 -0700

    BASIC SUBMISSIONS AND NEW BATTLES PER-SERVER ARE WORKING WOO

--- battlelib/battlecache.js
+++ battlelib/battlecache.js
@@ -1,39 +1,42 @@
 const fs = require('fs')
-
+const _cacheFile = 'battlecache.json'
+//TODO This needs to work for multiple serverNames from the start
 //TODO initialize code, read from _cacheFile if exists, put data in _entriesMap
+let battleMap = {}
 
-//TODO This needs to work for multiple servers from the start
+let debug = msg => console.log(`bcache: ${msg}`)
 
-exports.default = {
-  _cacheFile: 'battlecache.json',
-  battleMap: {},
-  addEntry: function(username, link, server){
-    let entryExists = false // For smarter output
-    if (!this.battleMap[server]){
-      this.battleMap[server] = {}
-    }
-    if (this.battleMap[server][username]) {
-      entryExists = true
-    }
-    this.battleMap[server][username] = link;
-    //TODO use fs.writeSync method since we're already async
-    if (entryExists){
-      return `thanks for the update, saved your new entry!`
-    } else {
-      return `welcome to the battle! >:D`
-    }
-  },
-  emptyCache: function(server){
-    //TODO mv disk cache to cache.back
-    if (this.battleMap[server]){
-      this.battleMap[server] = {}
-    }
-    //TODO save modified battlemap to disk
-  },
-  getCacheForServer: function(server){
-    if (!this.battleMap[server]){
-      this.battleMap[server] = {}
-    } 
-    return this.battleMap[server]
+exports.addEntry = function(username, link, serverName){
+  if (!battleMap[serverName]){
+    debug(`add: new subcache for ${serverName}`)
+    battleMap[serverName] = new Map() 
   }
+  let entryExisted = battleMap[serverName].get(username) // For smarter output
+  battleMap[serverName].set(username, link);
+  //TODO use fs.writeSync method since we're already async
+  if (entryExisted){
+    return `thanks for the update, saved your new entry! (${battleMap[serverName].size} entries)`
+  } else {
+    return `welcome to the battle! (${battleMap[serverName].size} entries)`
+  }
+}
+
+exports.emptyCache = function(serverName){
+  //TODO mv disk cache to cache.back
+  battleMap[serverName] = new Map()
+  return `a new battle has begun!`
+  //TODO save modified battlemap to disk
+}
+
+exports.getCacheForServer = function(serverName){
+  if (!battleMap[serverName]){
+    debug(`get: new subcache for ${serverName}`)
+    battleMap[serverName] = new Map()
+  } 
+  return battleMap[serverName]
+}
+
+exports.stopSubsForServer = function(serverName){
+  debug(`stop: TODO figure out way to cleanly stop entries for ${serverName}`)
+  return `this is not implemented yet`
 }
--- battlelib/battlecmds.js
+++ battlelib/battlecmds.js
@@ -1,49 +1,74 @@
 const bcache = require('./battlecache')
 
-exports.newbattle = function(input, msg, client) {
-  if (input.toLowerCase() == 'help') {
-    return `Usage: TODO usage info`
-  }
-  // TODO name with input, or give neat codename
-  bcache.emptyCache()
-  return `A NEW BATTLE HAS BEGUN!`
-}
+let debug = msg => console.log(`battlecmds: ${msg}`)
 
-exports.submit = function(input, msg, client) {
+exports.newbattle = function(input, msg) {
   if (input.toLowerCase() == 'help') {
     return `Usage: TODO usage info`
   }
-  if (msg.guild) {
-    // TODO delete msg, if msg.attach print Not supported yet, try bribing jake else save link
-    let requestorName = msg.member.user.username
-    bcache.addEntry(requestorName, input.trim())
-    return `your entry has been recorded (${bcache.entryMap.size} entries total)!`
+  if (msg.guild){
+    let serverName = msg.guild.name
+    return bcache.emptyCache(serverName)
   } else {
     return `this command needs to be run in a server, not a dm`
   }
 }
 
-exports.submissions = function(input, msg, client) {
+exports.submit = function(input, msg) {
   if (input.toLowerCase() == 'help') {
     return `Usage: TODO usage info`
   }
-  if (input === 'channel'){
-    return `channel output, `
+  if (msg.guild) {
+    let requestorName = msg.member.user.username
+    let serverName = msg.guild.name
+    return bcache.addEntry(requestorName, input.trim(), serverName)
+    // TODO delete msg, if msg.attach print Not supported yet, try bribing jake else save link
   } else {
-    return `dm output`
+    return `this command needs to be run in a server, not a dm`
   }
 }
 
-exports.stopsubs = function(input, msg, client){
+exports.submissions = function(input, msg) {
   if (input.toLowerCase() == 'help') {
     return `Usage: TODO usage info`
   }
-  return `submissions are CLOSED! (jk this is not )`
+  if (msg.guild) {
+    let serverName = msg.guild.name
+    debug(`channel, showing submissions for ${serverName}`)
+    const submissionMap = bcache.getCacheForServer(serverName)
+    return `TODO something more useful than this: \n\n\`\`\`${submissionMap}\`\`\``
+  } else {
+    debug(`dm, submissions for ${serverName}`)
+    if (input) {
+      let serverName = input
+      return ``
+    } else {
+      return `Usage: TODO dm usage info`
+    }
+  }
 }
 
-exports.battlevote = function(input, msg, client){
+exports.stopsubs = function(input, msg){
   if (input.toLowerCase() == 'help') {
     return `Usage: TODO usage info`
   }
-  return `here's a form with all entrants and emojis to vote on them with`
+  if (msg.guild) {
+    let serverName = msg.guild.name
+    return bcache.stopSubsForServer(serverName)
+  } else {
+    return `this command needs to be run in a server, not a dm`
+  }
+}
+
+exports.battlevote = function(input, msg){
+  if (input.toLowerCase() == 'help') {
+    return `Usage: TODO usage info`
+  }
+  if (msg.guild) {
+    let requestorName = msg.member.user.username
+    let serverName = msg.guild.name
+    return `here's a form with all entrants and emojis to vote on them with`
+  } else {
+    return `this command needs to be run in a server, not a dm`
+  }
 }
