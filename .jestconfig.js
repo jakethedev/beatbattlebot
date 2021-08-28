38eb221e65c88c19adbd74109adcdf8d20027318
jakethedev <admin@jakethedev.com>
2021-08-28 01:11:09 -0600

    No-op commit to note that I've verified that jest testing is an adequate fast syntax checker

--- .jestconfig.js
+++ .jestconfig.js
@@ -1,7 +1,4 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+/* https://jestjs.io/docs/configuration */
 
 module.exports = {
   // All imported modules in your tests should be mocked automatically
