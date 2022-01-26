62581ecdbe81b46745d0570e712e2cd9babc66b4
jakethedev <16889000+jakethedev@users.noreply.github.com>
2022-01-26 12:19:19 -0700

    Fixed jest test suite, all js files in test are run now

---
+++ tests/feedback/controllertests.js
@@ -0,0 +1,7 @@
+const con = require('../../commands/feedback/feedbackcontroller.js')
+
+test('feedback/feedbackcontroller:basic operation tests', () => {
+  console.log(`fbcontroller: ${con}`)
+})
+
+
---
+++ tests/feedback/daotests.js
@@ -0,0 +1,10 @@
+const dao = require('../../commands/feedback/feedbackdao.js')
+
+test('feedback/feedbackdao:basic operation tests', () => {
+  console.log(`fbdao: ${dao}`)
+  // // Check some handy setters and getters
+  // expect(dao.getPodiumSize()).toBe(15)
+  // dao.setPodiumSize(10)
+  // expect(dao.getPodiumSize()).toBe(10)
+})
+
