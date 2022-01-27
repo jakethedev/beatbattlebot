7e1344a59d7f517301723e57f68c78e5e586fd55
jakethedev <16889000+jakethedev@users.noreply.github.com>
2022-01-27 15:26:44 -0700

    More scaffolding per previous comment

--- util/servercachedao.js
+++ util/servercachedao.js
@@ -30,11 +30,11 @@ exports.ignoreChannel = function(discChannel) {
 }
 
 exports.getChannels = function(serverid) {
-  //TODO return [ channelids ] 
+  //TODO return [ channelids ]
 }
 
 exports.addBotModRole = function(serverid, discordJsRole) {
-  //TODO add, save, and be good 
+  //TODO add, save, and be good
 }
 
 exports.removeBotModRole = function(serverid, discordJsRole) {
@@ -47,11 +47,20 @@ exports.getBotModRoles = function(serverid) {
 
 _validTiebreakOptions = ['random', 'oldest', 'newest']
 exports.setTiebreak = function(serverid, input) {
-  //TODO validate input as 
+  //TODO validate input as
 }
 
 exports.getTiebreak = function(serverid) {
-
+  //TODO return a valid tiebreak option set for server
+  //    JIT save a default tiebreak upon first get
 }
 
 exports.getTiebreakOptions = () => _validTiebreakOptions
+
+exports.addWinner = function(serverid, channelid, userid) {
+  // TODO add a new champion
+}
+
+exports.getWinners = function(serverid, channelid) {
+  // TODO if serverid, get server winners. if channelid, get channel winners?
+}
