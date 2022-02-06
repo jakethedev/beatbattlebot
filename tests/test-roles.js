471bf9ba3bd6dc1ea3319f14d37d3d204743f6e4
jakethedev <16889000+jakethedev@users.noreply.github.com>
2022-02-05 18:10:39 -0700

    Package lock update, stub for discord role testing

---
+++ tests/test-roles.js
@@ -0,0 +1,7 @@
+test('discord/discordroles:giverole tests', () => {
+  const input = 'TODO this is incomplete' 
+  console.log(input)
+  expect(input).toBeTruthy()
+})
+// test('discord/discordroles:roles tests', () => {
+// test('discord/discordroles:reactrole tests', () => {
